78f198919d6c03f27993cc473ebfd530ab9c7188
JoseSPS <jlmartinez@spsolutions.com.mx>
2020-12-28 17:17:59 -0600

    Haciendo conexiones con interfaz

--- holaMundo/holaMundo/ViewController.swift
+++ holaMundo/holaMundo/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Hola"
+        print("primer print")
+        print("Segundo print")
+        print("tercer print")
     }
 
-
 }
-
