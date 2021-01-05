40bfecfea206a9685b73ff2b1c98a0c652d386f8
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-05 14:03:48 -0600

    Funcion print

---
+++ poo/poo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poo
+//
+//  Created by Jose Luis Espiritu on 28/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
