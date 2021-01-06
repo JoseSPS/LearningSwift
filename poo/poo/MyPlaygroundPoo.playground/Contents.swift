a3e339013a795f831ede585a1746fbe072f5024e
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-06 09:40:45 -0600

    POO Swift

--- poo/poo/MyPlaygroundPoo.playground/Contents.swift
+++ poo/poo/MyPlaygroundPoo.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var str = "Hello, playground"
+class Persona {
+    
+    var nombre:String = "Jose"
+    var edad:Int = 21
+    var colorCamisa:String = ""
+    
+    var color = Colores()
+    
+    func saludo() -> String{
+        colorCamisa = color.colorAzul
+        return "Mi nombre es \(nombre), mi edad es \(edad) años y mi camisa es de color \(colorCamisa)"
+    }
+    
+}
+
+class Colores {
+    var colorAzul:String = "Azul"
+    var colorRojo:String = "Rojo"
+}
+
+
+var objetoPersona = Persona()
+var nombreMiranda = objetoPersona.nombre
+objetoPersona.saludo()
