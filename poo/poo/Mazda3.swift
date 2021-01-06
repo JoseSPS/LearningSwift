0fd3c0555b39fe33adaf1aa9e27ef1751a068c8d
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-06 10:24:38 -0600

    Ejemplo POO aplicación grafica, instancias  y validaciones Optional

---
+++ poo/poo/Mazda3.swift
@@ -0,0 +1,24 @@
+//
+//  Mazda3.swift
+//  poo
+//
+//  Created by Jose Luis Espiritu on 06/01/21.
+//
+
+import Foundation
+
+
+class Mazda3 {
+    var tamaño = "Mediano"
+    var numeroPuertas = 4
+    var color = "Azul"
+    var precio = "20000 USD"
+    
+    func encender(){
+        print("El atomóvil fue encendido")
+    }
+    
+    func acelerar(){
+        print("El automóvil acelero")
+    }
+}
--- poo/poo/ViewController.swift
+++ poo/poo/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    var objetoMazda3:Mazda3?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelTitulo.text = "Fábrica de Automóviles"
     }
 
-
+    @IBAction func crearObjeto(_ sender: UIButton) {
+        print("Objeto creado")
+        objetoMazda3 = Mazda3()
+    }
+    
+    @IBAction func mostrarPropiedades(_ sender: UIButton) {
+        if objetoMazda3 != nil{
+            print("El carro Mazd 3 es de tamaño \(objetoMazda3!.tamaño), tiene \(objetoMazda3!.numeroPuertas) puertas, su color es \(objetoMazda3!.color) y su precio es de \(objetoMazda3!.precio)")
+        } else {
+            print("No se ha creado el objeto")
+        }
+    
+    }
+    
+    @IBAction func encender(_ sender: UIButton) {
+        if objetoMazda3 != nil{
+            objetoMazda3!.encender()
+        } else {
+            print("No se ha creado el objeto")
+        }
+    }
+    
+    @IBAction func acelerar(_ sender: UIButton) {
+        if objetoMazda3 != nil{
+            objetoMazda3!.acelerar()
+        } else {
+            print("No se ha creado el objeto")
+        }
+    }
+    
 }
 
